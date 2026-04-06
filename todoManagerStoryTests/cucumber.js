0965effa37c6c7deb84bb0a60694d068e60b5083
ashsnandi <ash.nandi@mail.mcgill.ca>
2026-04-06 12:30:39 -0400

    cleaned repo up for part 3, added test automation for proejcts

--- todoManagerStoryTests/cucumber.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  default: {
-    paths: ["features/**/*.feature"],
-    require: ["steps/**/*.js"],
-    order: "random",
-    format: ["progress-bar", "html:reports/cucumber-report.html"],
-  },
-};
